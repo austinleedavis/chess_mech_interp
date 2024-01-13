66e00f47e04140f1e130c870933a00fe6bd3ba1c
Austin <austindavis@gmail.com>
2024-01-12 21:13:20 -0500

    fixed slurm filename and extra whitespace

--- slurm_process_dataset_maker.py
+++ slurm_process_dataset_maker.py
@@ -2,30 +2,32 @@
 import os
 
 def get_slurm_script_string(CHUNK_MIN,CHUNK_MAX):
-    return f"""#!/bin/bash
-    #SBATCH --nodes=1
-    #SBATCH --time=10:00
-    #SBATCH --mem=50G
-    #SBATCH --job-name=uci{CHUNK_MIN}-{CHUNK_MAX}
-    #SBATCH --error=slurm/opgn2uci-err-{CHUNK_MIN:03}-{CHUNK_MAX:03}-%J.err
-    #SBATCH --output=slurm/pgn2uci-out-{CHUNK_MIN:03}-{CHUNK_MAX:03}-%J.out
+    script = (
+f"""#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=10:00
+#SBATCH --mem=50G
+#SBATCH --job-name=uci{CHUNK_MIN}-{CHUNK_MAX}
+#SBATCH --error=slurm/opgn2uci-err-{CHUNK_MIN:03}-{CHUNK_MAX:03}-%J.err
+#SBATCH --output=slurm/pgn2uci-out-{CHUNK_MIN:03}-{CHUNK_MAX:03}-%J.out
 
-    module load anaconda/anaconda3
+module load anaconda/anaconda3
 
-    source /apps/anaconda/anaconda3/etc/profile.d/conda.sh 2> /dev/null
+source /apps/anaconda/anaconda3/etc/profile.d/conda.sh 2> /dev/null
 
-    conda activate base 
+conda activate base 
 
-    conda activate chess
+conda activate chess
 
-    which python
+which python
 
-    hostname
+hostname
 
-    pwd
+pwd
 
-    python src/prepare_dataset.py --chunk_min {CHUNK_MIN} --chunk_max {CHUNK_MAX}
-    """
+python src/prepare_dataset.py --chunk_min {CHUNK_MIN} --chunk_max {CHUNK_MAX}
+""")
+    return script
 
 DIR = "slurm/data_preprocessing/"
 
@@ -33,10 +35,10 @@ if not os.path.exists(DIR):
     os.makedirs(DIR)
 
 slurm_files = []
-for i in range(165):
+for i in range(10):
     script = get_slurm_script_string(i,i+1)
     
-    filename = DIR+'process_lichess_chunk_{i:03}.slurm'
+    filename = DIR+f'process_lichess_chunk_{i:03}.slurm'
     
     slurm_files.append(filename)
     
